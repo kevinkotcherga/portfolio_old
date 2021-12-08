2da86d3592af213832f314ba1258b9ebc9492b7a
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2021-12-08 15:54:21 +0100

    Home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_102341) do
+ActiveRecord::Schema.define(version: 2021_12_08_144247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "preferences", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
